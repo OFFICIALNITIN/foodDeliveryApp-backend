2e00080a78d1b4ae6cf95aa9e5b5cd8077eed4ee
OFFICIALNITIN <official.nitin105@gmail.com>
2024-03-29 23:02:23 +0530

    added cors middleware

--- food-delivery-app/index.js
+++ food-delivery-app/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const priceRouter = require('./src/routes/priceRouter.js')
+const cors = require('cors')
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 
 //Routes
 app.use("/api/",priceRouter)
